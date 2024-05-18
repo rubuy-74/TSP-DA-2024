009e94535dd594f130405669221e955c29bc0d53
rubuy-74 <rubemviscard2635@gmail.com>
2024-05-18 17:05:34 +0100

    Add Documentation

--- classes/Heuristic.h
+++ classes/Heuristic.h
@@ -12,9 +12,31 @@
 
 class Heuristic {
 public:
+    /**
+     * Dijkstra algorithm to determine the smallest distance between all the nodes
+     * @param graph graph to be analyzed
+     * @param oddDegreeNodes nodes with odd degree
+     * @return a map with the current node and the next and the distance between them
+     */
     static std::unordered_map<int, std::unordered_map<int, double>> djikstra(Graph *graph, std::set<int> oddDegreeNodes);
+    /**
+     * Perfect matching algorithm to group for groups of two to minimize its distance.
+     * @param graph graph to be analyzed
+     * @param oddDegreeNodes  nodes with odd degree
+     * @return Modified graph
+     */
     static Graph* minimumWeightPerfectMatching(Graph *graph, std::set<int> oddDegreeNodes);
+    /**
+     * Determines an Eulerian Circuit
+     * @param graph graph to be analyzed
+     * @return Eulerian Circuit
+     */
     static std::vector<int> findEulerianCircuit(Graph *graph);
+    /**
+     * Returns the final cost and path
+     * @param graph graph to be analyzed
+     * @return map of the final cost and path
+     */
     static std::pair<double, std::vector<int>> heuristic(Graph *graph);
 };
 
--- classes/Triangular.h
+++ classes/Triangular.h
@@ -9,8 +9,23 @@
 
 
 class Triangular {
+    /**
+     * @brief Create a circuit from the MST tree and the distances on the original graph.
+     * @param node first node to be analyzed (root)
+     * @param graph MST tree of the original graph
+     * @param oldGraph Graph before the MST (original graph)
+     * @param result Path of the circuit
+     * @param sumDist Final sum of Distances
+     */
     static void preOrderGraph(Node* node, Graph graph, Graph oldGraph, std::vector<int> &result, int &sumDist);
 public:
+    /**
+     * @brief Algorithm that determines an approximation to the optimal solution on the TSP algorithm
+     * Based on the triangular inequality (the smallest distance between two nodes is a line), it is possible to determine a good approximation to the optimal solution for the TSP problem.
+     * @param graph Graph to be analyzed
+     * @param result Path of the circuit
+     * @return Final sum of Distances (cost of the path)
+     */
     static double tspTriangular(Graph graph,std::vector<int> &result);
 };
 
